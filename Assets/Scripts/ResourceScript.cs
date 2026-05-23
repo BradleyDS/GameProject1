b3f00f50c24c343164a43a2f245e6686bf77f7f4
BradleyDS <149975068+BradleyDS@users.noreply.github.com>
2026-05-23 15:43:51 -0500

    Added a glimmer increase
    Includes passive and when clicked very basic rn

---
+++ Assets/Scripts/ResourceScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ResourceScript : MonoBehaviour
+{
+    //This script handles passive glimmer income and glimmer income when clicked (Decrease isnt done yet that will be later)
+    public float glimmerNum = 0f;
+    public float passiveIncrease = 0f;
+    public float glimmerDecrease = 0f;
+    public float clickIncrease = 0f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GainGlimmer();
+        print(glimmerNum);
+    }
+    public void GainByClick()
+    {
+        glimmerNum += clickIncrease;
+    }
+    public void GainGlimmer()
+    {
+        glimmerNum += passiveIncrease * Time.deltaTime;
+
+    }
+    public void LoseGLimmer()
+    {
+        glimmerNum -= glimmerDecrease = 0f;
+
+    }
+}
