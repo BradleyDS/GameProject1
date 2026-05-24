e6af6c0a85b8195ee957d1b527f61abc39a31e6e
BradleyDS <149975068+BradleyDS@users.noreply.github.com>
2026-05-24 15:53:48 -0500

    Early upgrade button

--- Assets/Scripts/GlimShopScript.cs
+++ Assets/Scripts/GlimShopScript.cs
@@ -1,16 +1,49 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GlimShopScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float glimAmount = 100f;
+    public float passiveIncrease = 0f;
+    public float upgradeCost = 100f;
+    private Button thisButton;
+
+
+    private void Start()
     {
-        
+        thisButton = GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        GainGlimmer();
+        //print(glimAmount);
+
+        if (glimAmount < upgradeCost)
+        {
+            thisButton.interactable = false;
+            
+        }
+        else if (glimAmount > upgradeCost)
+        {
+            thisButton.interactable = true;
+        }
+     
+
     }
+
+    public void ButtonUpgrade()
+    {
+        glimAmount -= upgradeCost;
+        passiveIncrease += 10f;
+        upgradeCost += 50f;
+    }
+
+    public void GainGlimmer()
+    {
+        glimAmount += passiveIncrease * Time.deltaTime;
+
+    }
+
 }
